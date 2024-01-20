b7991128b7fba1f8381f38d8d78280eda93e3395
Akshita <agarwalakshita2003@gmail.com>
2024-01-20 18:33:56 +0530

    sql connection established

---
+++ src/main/java/com/blog/tech/helper/ConnectionProvider.java
@@ -0,0 +1,23 @@
+package com.blog.tech.helper;
+
+import java.sql.*;
+
+public class ConnectionProvider {
+	private static Connection con;
+	
+	public static Connection getConnection() {
+		try {
+			if(con==null) {
+				//driver class load..
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				//create a connection..
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techblog", "root", "Durga@123");
+				
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	
+<%@ page import="java.sql.*" %>	
+<%@ page import="com.blog.tech.helper.ConnectionProvider" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +16,13 @@
 <body>
 
 	<h1>Hello World</h1>
-	<div id="testElement">Loading...</div>
+	
+	<%
+	Connection con = ConnectionProvider.getConnection();
+	
+	%>
+	
+	<h1><%=con %></h1>
 
 
 	<!-- javascripts -->
